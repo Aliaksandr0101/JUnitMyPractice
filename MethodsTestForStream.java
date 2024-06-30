257f12c496f1f31fb18f0bec4d4a55ef1c62613a
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-06-30 22:58:44 +0300

    Commit Five. Add method enterOnDisplay amd interface cheker

--- MethodsTestForStream.java
+++ MethodsTestForStream.java
@@ -1,2 +1,14 @@
 public class MethodsTestForStream {
+
+   static void enterOnDisplay( Checker ch){
+        System.out.println(ch.checkOnNumberOfLetter("Mountain"));
+    }
+
+    public static void main(String[] args) {
+        enterOnDisplay((String anyString) -> {return anyString.length() > 3;});
+    }
+
+}
+interface Checker{
+    boolean checkOnNumberOfLetter(String newString);
 }
