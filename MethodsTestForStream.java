c23c8e96337f6041077117490815c448a65f5a7b
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-07-02 22:54:39 +0300

    Commit Six. change method enterOnDisplay and interface cheker two

--- MethodsTestForStream.java
+++ MethodsTestForStream.java
@@ -1,12 +1,13 @@
 public class MethodsTestForStream {
 
    static void enterOnDisplay( Checker ch){
-        System.out.println(ch.checkOnNumberOfLetter("Mountain"));
+
+       System.out.println(ch.checkOnNumberOfLetter("Mountain"));
     }
 
     public static void main(String[] args) {
         enterOnDisplay((String anyString) -> {return anyString.length() < 3;});
-
+        Checker newChecker = (String anyString2) -> {return anyString2.length() > 3;};
     }
 
 }
