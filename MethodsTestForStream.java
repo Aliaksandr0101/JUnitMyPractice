14ab39f525841d9c72fbece792927d463c701f9a
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-07-05 23:07:45 +0300

    Commit Six. change method main in class MethodsTestForStream. Part three

--- MethodsTestForStream.java
+++ MethodsTestForStream.java
@@ -10,7 +10,14 @@ public class MethodsTestForStream {
         Checker newChecker =  newString -> newString.length() < 1;
         boolean result = newChecker.checkOnNumberOfLetter("Hello");
         System.out.println(result);
-    }
+
+       new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("Я только что реализовал функциональный интерфейс Runnable.");
+        }
+    }).start();
+}
 
 }
 interface Checker{
