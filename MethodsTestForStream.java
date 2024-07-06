b3df951a405e502f11380e947877a0a216c739ed
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-07-06 21:57:44 +0300

    Commit Six. change method main in class MethodsTestForStream. Part four

--- MethodsTestForStream.java
+++ MethodsTestForStream.java
@@ -10,6 +10,7 @@ public class MethodsTestForStream {
         Checker newChecker =  newString -> newString.length() < 1;
         boolean result = newChecker.checkOnNumberOfLetter("Hello");
         System.out.println(result);
+        System.out.println(result);
 
        new Thread(new Runnable() {
         @Override
